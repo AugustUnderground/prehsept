379ce63c2e003ea190c000d36810f6c8b73b9457
Yannick Uhlmann <augustunderground@protonmail.com>
2023-05-30 10:45:12 +0200

    fix up train script

--- train.sh
+++ train.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-# PDK="xh035 xh018 xt018 gpdk180 gkd090 gpdk045"
-PDK="xh018 xt018"
+PDK="xh018 xt018 gpdk180 gpdk090 gpdk045"
 DEV="nmos pmos"
 
 for pdk in $(echo "$PDK"); do
     for dev in $(echo "$DEV"); do
-        stack exec -- prehsept-exe --pdk $pdk --dev $dev --dir "./data/$pdk-$dev.pt" --size 256 --num 12
+        stack exec -- prehsept-exe --pdk $pdk --dev $dev --dir "./data/$pdk-$dev.pt" --size 32 --num 100 --reg 2
     done
 done
